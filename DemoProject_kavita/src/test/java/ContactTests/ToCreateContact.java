5c5ed0c2384f73abb23ef223fcc91820127abe2d
hp <hp@LAPTOP-L3BQ5DRJ>
2025-04-01 09:36:54 +0530

    Test script written

--- DemoProject_kavita/src/test/java/ContactTests/ToCreateContact.java
+++ DemoProject_kavita/src/test/java/ContactTests/ToCreateContact.java
@@ -4,6 +4,7 @@ public class ToCreateContact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test script for create contact is written successfully by shreya");
 		
 		
 
